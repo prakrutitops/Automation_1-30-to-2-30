19a9206b617a0cd587ca82e2655271e0291067db
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2023-06-30 14:17:16 +0530

    Add files via upload

---
+++ Module3/30-6/MyTest.java
@@ -0,0 +1,33 @@
+package crossbrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class MyTest 
+{
+	@Parameters("browser")
+	@Test
+	public void test(String browser)
+	{
+		
+		WebDriver driver=null;
+		
+		if(browser.equals("chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver", "E:\\chromedriver\\chromedriver.exe");		
+			driver = new ChromeDriver();
+		}
+		else if(browser.equals("ff"))
+		{
+			System.setProperty("webdriver.gecko.driver","E:\\chromedriver\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		
+		driver.get("https://topsint.com/topserp/index.php");
+		
+	}
+	
+}
